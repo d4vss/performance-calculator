94645921b8c8bab457b0efa0357bba448e0b0cdd
d4vss <godflod@gmx.at>
2023-04-28 10:34:02 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page">
-        <h2>Home</h2>
+        <h2>Performance calculator</h2>
         <div>
             <p>
                 This is a A320 performance calculator made <span class="underline">only</span> for games.
@@ -9,7 +9,7 @@
                 It is a part of a series of projects I am building to learn Vue.js.
             </p>
             <p>
-                The source code is available on <a class="hover-underline-animation" href="https://github.com/d4vss/performance-calculator">GitHub</a>.
+                The source code is available on <a class="hover-underline-animation" target=_blank href="https://github.com/d4vss/performance-calculator">GitHub</a>.
             </p>
         </div>
     </div>
